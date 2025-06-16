d59f4b26f844da3a682d1933ff53818187735f99
AhmedAbdulla22 <AhmedAbdulla22@users.noreply.github.com>
2025-06-16 18:01:09 +0300

    add dynamic size, remove, constraint

--- index.js
+++ index.js
@@ -1,36 +1,72 @@
+const btnGridSize = document.querySelector('#btn-grid-size');
 const divsContainer = document.querySelector('#divs-container');
-//create 16x16 div grid
-//create rows & cols
-const gridRow = document.createElement("div");
-gridRow.classList.add("grid-row");
 
-const gridCell = document.createElement("div");
-gridCell.classList.add("grid-cell");
+//set new grid
+setNewGrid();
 
-for (let index = 0; index < 16; index++) {
-    //add a row
-    const gridRowCopy = gridRow.cloneNode(false);
-    gridRowCopy.id = `grid-row-${index}`;
-    divsContainer.appendChild(gridRowCopy);
-
-    //add 16 cell to each new row
-    for (let index = 0; index < 16; index++) {
-        const gridCellCopy = gridCell.cloneNode(false);
-        gridCellCopy.id = `grid-cell-${index}`;
-        gridRowCopy.appendChild(gridCellCopy);
-    }
-}
 
 function changeCellColor(cell) {
     cell.style.backgroundColor = "black";
 }
 
-divsContainer.addEventListener("mouseover",(e) => {
-    console.log(e.target.tagName);
-    if(e.target.tagName !== "DIV") return;
-    changeCellColor(e.target);
+function removeTheGrid() {
+    divsContainer.replaceChildren();
+}
+
+function changeGridSize() {
+    let numOfCellsPerSide = Number(prompt("number of squares per side for the new grid\nrange:16-100"));
     
-})
+    //constraint
+    if(numOfCellsPerSide > 100) {
+        numOfCellsPerSide = 100;
+    } else if (numOfCellsPerSide < 10) {
+        numOfCellsPerSide = 16;
+    }
+
+    //remove current grid
+    removeTheGrid();
+
+    setNewGrid(numOfCellsPerSide);
+}
+
+function setNewGrid(cellPerRow = 16) {
+    //create div grid (16x16 default) per side
+    //create rows & cols
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+
+    const gridSize = 600;
+    const cellSize = Math.floor(gridSize / cellPerRow);
+    const gridCell = document.createElement("div");
+    gridCell.classList.add("grid-cell");
+    gridCell.style.cssText = 
+    `outline: 1px solid black;
+    width: ${cellSize}px;
+    height: ${cellSize}px;`;
+
+    for (let index = 0; index < cellPerRow; index++) {
+        //add a row
+        const gridRowCopy = gridRow.cloneNode(false);
+        gridRowCopy.id = `grid-row-${index}`;
+        divsContainer.appendChild(gridRowCopy);
+
+        //add cells to each new row
+        for (let index = 0; index < cellPerRow; index++) {
+            const gridCellCopy = gridCell.cloneNode(false);
+            gridCellCopy.id = `grid-cell-${index}`;
+            gridRowCopy.appendChild(gridCellCopy);
+        }
+    }
+}
+
+//mouse hover cell effect
+divsContainer.addEventListener("mouseover",(e) => {
+    if(!e.target.classList.contains("grid-cell")) return;
+    changeCellColor(e.target);
+});
+
+//change grid size
+btnGridSize.addEventListener("click",changeGridSize); 
 
 
 
